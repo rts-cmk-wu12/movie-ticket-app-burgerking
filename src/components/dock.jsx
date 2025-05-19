2719deecfed54e7b7d80dbddf826de59fb062fd9
GR0UD <marksgalkins12345678910@gmail.com>
2025-05-19 13:07:35 +0200

    BRUH

--- src/components/dock.jsx
+++ src/components/dock.jsx
@@ -1,20 +1,29 @@
 import Icons from "../utils/Icons";
+import { NavLink } from "react-router-dom";
 
 export default function Dock() {
   return (
     <nav className='dock-nav' aria-label='Bottom Navigation'>
       <ul className='dock-list'>
         <li>
-          <Icons.home size={24} aria-label='Home' />
+          <NavLink to='/' className='dock-link'>
+            <Icons.home size={24} aria-label='Home' />
+          </NavLink>
         </li>
         <li>
-          <Icons.compass size={24} aria-label='Explore' />
+          <NavLink to='/explore' className='dock-link'>
+            <Icons.compass size={24} aria-label='Explore' />
+          </NavLink>
         </li>
         <li>
-          <Icons.bookmark size={24} aria-label='Bookmarks' />
+          <NavLink to='/bookmarks' className='dock-link'>
+            <Icons.bookmark size={24} aria-label='Bookmarks' />
+          </NavLink>
         </li>
         <li>
-          <Icons.user size={24} aria-label='Profile' />
+          <NavLink to='/profile' className='dock-link'>
+            <Icons.user size={24} aria-label='Profile' />
+          </NavLink>
         </li>
       </ul>
     </nav>
