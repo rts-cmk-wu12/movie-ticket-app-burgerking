bcd4f0a43376aca725124683597aeed1d5b3eef0
GR0UD <marksgalkins12345678910@gmail.com>
2025-05-19 13:21:51 +0200

    small stuff

--- src/components/dock.jsx
+++ src/components/dock.jsx
@@ -6,22 +6,42 @@ export default function Dock() {
     <nav className='dock-nav' aria-label='Bottom Navigation'>
       <ul className='dock-list'>
         <li>
-          <NavLink to='/' className='dock-link'>
+          <NavLink
+            to='/'
+            className={({ isActive }) =>
+              `dock-link${isActive ? " active" : ""}`
+            }
+          >
             <Icons.home size={24} aria-label='Home' />
           </NavLink>
         </li>
         <li>
-          <NavLink to='/explore' className='dock-link'>
+          <NavLink
+            to='/explore'
+            className={({ isActive }) =>
+              `dock-link${isActive ? " active" : ""}`
+            }
+          >
             <Icons.compass size={24} aria-label='Explore' />
           </NavLink>
         </li>
         <li>
-          <NavLink to='/bookmarks' className='dock-link'>
+          <NavLink
+            to='/bookmarks'
+            className={({ isActive }) =>
+              `dock-link${isActive ? " active" : ""}`
+            }
+          >
             <Icons.bookmark size={24} aria-label='Bookmarks' />
           </NavLink>
         </li>
         <li>
-          <NavLink to='/profile' className='dock-link'>
+          <NavLink
+            to='/profile'
+            className={({ isActive }) =>
+              `dock-link${isActive ? " active" : ""}`
+            }
+          >
             <Icons.user size={24} aria-label='Profile' />
           </NavLink>
         </li>
