9a7f0b10a882ecff7ad9f6f649d64c83c65c452e
Johndillerman <U2486892@rts-365.dk>
2025-05-19 13:15:19 +0200

    font family utils and explore page

--- src/pages/explore.jsx
+++ src/pages/explore.jsx
@@ -1,21 +1,44 @@
 import Layout from "../components/layout";
- 
+import { useEffect, useState } from "react";
+import "../styles/pages/_explore.scss"; 
 export default function Explore() {
-    return (
-        <Layout>
-            <div className="tabs">
-                <button className="tabs__button-showing">now showing</button>
-                <button className="tabs__button-upcoming">ppcoming</button>
-            </div>
-            <section className="top-movies">
-                <h2 className="top-movies__title">Top Movies</h2>
-                <div className="top-movies__list">
-                    <div className="top-movies__item">
-                        <img src="" alt="Movie Poster" />
-                        <h3 className="top-movies__item-title">Movie Title</h3>
-                    </div>
+        const [movies, setMovies] = useState([]);
+
+        useEffect(() => {
+            async function fetchMovies() {
+                try {
+                    const response = await fetch(
+                        "https://api.themoviedb.org/3/movie/top_rated?api_key=d53faa548947914998a2cd2461c8ae72"
+                    );
+                    const data = await response.json();
+                    setMovies(data.results);
+                } catch (error) {
+                    console.error("Error fetching movies:", error);
+                }
+            }
+            fetchMovies();
+        }, []);
+
+        return (
+            <Layout>
+                <div className="tabs">
+                    <button className="tabs__button">now showing</button>
+                    <button className="tabs__button">upcoming</button>
                 </div>
-            </section>
-        </Layout>
-    );
-}
+                <section className="top-movies">
+                    <h2 className="top-movies__title">top movies</h2>
+                    <div className="top-movies__list">
+                        {movies.map((movie) => (
+                            <div key={movie.id} className="top-movies__item">
+                                <img
+                                    src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                                    alt={movie.title}
+                                />
+                                <h3 className="top-movies__item-title">{movie.title}</h3>
+                            </div>
+                        ))}
+                    </div>
+                </section>
+            </Layout>
+        );
+    }
