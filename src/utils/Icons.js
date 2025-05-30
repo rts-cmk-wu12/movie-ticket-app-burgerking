c83ff1ddd820884a040107d147f67c38bd0715dd
GR0UD <marksgalkins12345678910@gmail.com>
2025-05-30 13:33:16 +0200

    everything done just needs to be modified and fixed component modal needs to be made. but we did it the static boring way this time :)

--- src/utils/Icons.js
+++ src/utils/Icons.js
@@ -2,6 +2,7 @@ import { FaHome, FaCompass, FaBookmark, FaUser } from "react-icons/fa";
 import { HiSearch } from "react-icons/hi";
 import { RiShieldCheckFill } from "react-icons/ri";
 import { FaStar } from "react-icons/fa6";
+import { FaFileDownload } from "react-icons/fa";
 
 const Icons = {
   home: FaHome,
@@ -11,6 +12,7 @@ const Icons = {
   search: HiSearch,
   shield: RiShieldCheckFill,
   star: FaStar,
+  download: FaFileDownload,
 };
 
 export default Icons;
